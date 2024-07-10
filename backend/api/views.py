ccbe63d6d0a94464527eab8c216b6824b40e47d6
gogicha007 <gogicha@gmail.com>
2024-07-10 22:31:42 +0400

    chore: save the progress

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,3 +9,9 @@ class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+    
+
+class GetUserView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
