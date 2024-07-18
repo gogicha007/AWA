70673cbfb7f89d20d63b5e01e0b01c2c1fedb851
gogicha007 <gogicha@gmail.com>
2024-07-18 17:34:28 +0400

    chore: save the progress

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,31 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const authOptions: NextAuthOptions = {
-    providers: []
-}
-const handler = NextAuth(authOptions)
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: {
+          label: 'user',
+          type: 'text',
+        },
+        password: {
+          label: 'password',
+          type: 'password',
+        },
+      },
+      async authorize(credentials): Promise<any> {
+        const res = await fetch("/your/endpoint", {
+            method: 'POST',
+            body: JSON.stringify(credentials),
+            headers: { "Content-Type": "application/json" }
+          })
+          const user = await res.json()
+      }
+    }),
+  ]
+};
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,15 +1,23 @@
 'use client';
 import React from 'react';
+import Link from 'next/link';
 import styles from './header.module.css';
 import Image from 'next/image';
 import Navbar from '../navbar/navbar';
 import Logo from '../../../public/images/chuck.png';
 
-
 const Header = () => {
   return (
     <div className={styles.wrapper}>
-      <Image src={Logo} priority={false} width={100} height={100} alt="logo" />
+      <Link href="/">
+        <Image
+          src={Logo}
+          priority={false}
+          width={100}
+          height={100}
+          alt="logo"
+        />
+      </Link>
       <Navbar />
       <div className={styles.auth}>
         <button className={styles.btn}>Log In</button>
