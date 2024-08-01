4da36f74e87dc22692bd023792b606577e7ea7c7
gogicha007 <gogicha@gmail.com>
2024-08-01 12:53:41 +0400

    chore: save the progress

--- frontend/src/components/alert-modal/alert-modal.tsx
+++ frontend/src/components/alert-modal/alert-modal.tsx
@@ -1,11 +1,18 @@
+'use client'
 import styles from './alert-modal.module.css'
+import { useRef } from 'react'
 
-const AlertModal = () => {
+type Props = {
+    title: string
+}
+export default function AlertModal({title}: Props) {
+    const dialogRef = useRef<null | HTMLDialogElement>(null)
     return (
-        <div className={styles.modal}>
+        <dialog ref={ dialogRef} className={styles.modal}>
             <div className={styles.modal__container}>
-                
+                <h1>{title}</h1>
+                <button>Ok</button>
             </div>
-        </div>
+        </dialog>
     )
 }
