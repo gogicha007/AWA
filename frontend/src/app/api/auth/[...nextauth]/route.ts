4e0844135d3785de3b1c7527e03ab29852c13c25
gogicha007 <gogicha@gmail.com>
2024-09-08 19:37:42 +0400

    chore: save the progress

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -3,8 +3,16 @@ import { jwtDecode } from 'jwt-decode';
 import { endpointObj } from '@/lib/endpoints';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
+interface Token {
+  username: string;
+  email: string;
+  user_id: number;
+  exp: number;
+  is_superuser: boolean;
+  is_staff: boolean;
+}
+
 export const authOptions: NextAuthOptions = {
-  session: { strategy: 'jwt' },
   providers: [
     CredentialsProvider({
       name: 'Credentials',
@@ -18,17 +26,27 @@ export const authOptions: NextAuthOptions = {
           body: JSON.stringify(credentials),
           headers: { 'Content-Type': 'application/json' },
         });
-        const user = await res.json();
-        const token = jwtDecode(user.refresh);
+        const token = await res.json();
+        if (res.status !== 200) return null;
+        console.log(jwtDecode(token.access));
+        const { username, email, user_id, exp, is_superuser, is_staff } =
+          jwtDecode(token.access) as Token;
         console.log(token);
-        if (user) {
-          return user;
-        } else {
-          return null;
-        }
+        return {
+          ...token,
+          exp,
+          user: {
+            username,
+            email,
+            user_id,
+            is_staff,
+            is_superuser,
+          },
+        };
       },
     }),
   ],
+  session: { strategy: 'jwt' },
 };
 const handler = NextAuth(authOptions);
 
