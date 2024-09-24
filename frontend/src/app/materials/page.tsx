916da6232b9cf1f1f7c03de0b63243c769136e67
gogicha007 <gogicha@gmail.com>
2024-09-24 22:05:24 +0400

    chore: save the progress

--- frontend/src/app/materials/page.tsx
+++ frontend/src/app/materials/page.tsx
@@ -1,5 +1,28 @@
-import styles from './page.module.css'
+import Link from 'next/link';
+import styles from './page.module.css';
 
-export default function Materials(){
-    return <h1>here comes the Materials</h1>
+export default function Materials() {
+  return (
+    <div className={styles.materials}>
+      <ul className={styles.materials__navbar}>
+        <li>
+          <Link className={styles.materials__btn} href={'/materials/purchases'}>
+            Purchases
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={styles.materials__btn}
+            href={'/materials/wharehouse'}
+          >
+            Wharehouse
+          </Link>
+        </li>
+      </ul>
+      <div>
+        
+      </div>
+      <h1>here comes the Materials</h1>
+    </div>
+  );
 }
---
+++ frontend/src/app/materials/purchases/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import styles from './page.module.css';
+
+export default function Purchases() {
+  return (
+    <div className={styles.purchases}>
+      <h1>here comes the Purchases</h1>
+    </div>
+  );
+}
