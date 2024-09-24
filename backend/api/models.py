1135478e891842fd2a1f508de2423069fb552eb7
gogicha007 <gogicha@gmail.com>
2024-09-24 17:51:18 +0400

    chore: save the progress

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,6 +2,78 @@ from django.contrib.gis.db import models
 from django.contrib.auth.models import User
 
 
+class Purchase(models.Model):
+    MATERIALS = [
+        ('PE100', 'PE100'),
+        ('STEEL', 'STEEL'),
+        ('STAINLESS_STEEL', 'STAINLESS STEEL'),
+        ('CI', 'CI'),
+        ('NA', 'NA')
+    ]
+    UNIT = [
+        ('PCS', 'PCS'),
+        ('M', 'M'),
+        ('KG', 'KG'),
+    ]
+    CATEGORIES = [
+        ('PIPE', 'PIPE'),
+        ('VALVE', 'VALVE'),
+        ('FITTING', 'FITTING'),
+        ('STRAINER', 'STRAINER'),
+        ('DISMANTLING_JOINT', 'DISMANTLING JOINT'),
+        ('OTHER', 'OTHER')
+    ]
+    PN = [
+        ('8', '8'),
+        ('10', '10'),
+        ('16', '16'),
+        ('20', '20'),
+        ('25', '25'),
+        ('40', '40'),
+        ('65', '65'),
+        ('NA', 'NA')
+    ]
+    CURRENCY = [
+        ('USD', 'USD'),
+        ('EUR', 'EUR')
+    ]
+    name = models.CharField(max_length=50)
+    material = models.CharField(
+        max_length=15, choices=MATERIALS, default='PE100')
+    type = models.CharField(
+        max_length=17, choices=CATEGORIES, default='OTHER'
+    )
+    dn = models.IntegerField(null=True)
+    pn = models.CharField(
+        max_length=3, choices=PN, default='NA')
+    decl_no = models.CharField(max_length=7)
+    decl_date = models.DateField
+    seller = models.CharField(max_length=30)
+    truck = models.CharField(max_length=12)
+    invoice = models.CharField(max_length=20)
+    inv_date = models.DateField
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    quantity = models.DecimalField(
+        max_digits=9, decimal_places=2, null=True, blank=True)
+    measure = models.CharField(
+        max_length=5, choices=UNIT, default='PCS'
+    )
+    price = models.DecimalField(
+        max_digits=9, decimal_places=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        max_length=3, choices=CURRENCY, default='EUR'
+    )
+
+    class Meta:
+        db_table = 'purchase'
+        ordering = ['material', 'dn']
+
+    def __str__(self):
+        return f'{self.material}, DN{str(self.dn)}'
+
+
 class Materials(models.Model):
     MATERIALS = [
         ('PE100', 'PE100'),
@@ -19,13 +91,13 @@ class Materials(models.Model):
         ('OTHER', 'OTHER')
     ]
     PNS = [
-        ('8','8'),
+        ('8', '8'),
         ('10', '10'),
         ('16', '16'),
         ('20', '20'),
         ('25', '25'),
         ('40', '40'),
-        ('63', '63'),
+        ('65', '65'),
         ('NA', 'NA')
     ]
     name = models.CharField(max_length=50)
@@ -36,7 +108,7 @@ class Materials(models.Model):
     )
     pn = models.CharField(
         max_length=17, choices=PNS, default='NA')
-    
+
     dn = models.IntegerField(null=True)
     od = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
@@ -49,8 +121,10 @@ class Materials(models.Model):
     def __str__(self):
         return f'{self.material}, DN{str(self.dn)}'
 
+
 class Suppliers(models.Model):
     name = models.CharField(max_length=20)
+
 
 class Purchases(models.Model):
     invoice_no = models.CharField(max_length=20)
@@ -59,8 +133,10 @@ class Purchases(models.Model):
     def __str__(self):
         return str(self.id)
 
+
 class Nodes(models.Model):
     name = models.CharField(max_length=20)
+
 
 class Junctions(models.Model):
     geom = models.PointField()
