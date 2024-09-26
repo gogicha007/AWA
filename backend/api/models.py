7d817d6cf91cf991b36ae232fea2580f5874b18d
gogicha007 <gogicha@gmail.com>
2024-09-26 12:57:23 +0400

    chore: save the progress

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.contrib.gis.db import models
 from django.contrib.auth.models import User
 
@@ -20,7 +21,7 @@ class Purchase(models.Model):
         ('VALVE', 'VALVE'),
         ('FITTING', 'FITTING'),
         ('STRAINER', 'STRAINER'),
-        ('PUMP','PUMP'),
+        ('PUMP', 'PUMP'),
         ('DISMANTLING_JOINT', 'DISMANTLING JOINT'),
         ('OTHER', 'OTHER')
     ]
@@ -47,15 +48,15 @@ class Purchase(models.Model):
     dn = models.IntegerField(null=True)
     pn = models.CharField(
         max_length=3, choices=PN, default='NA')
-    decl_no = models.CharField(max_length=7)
-    decl_date = models.DateField(null=True, blank=True)
-    seller = models.CharField(max_length=30)
-    truck = models.CharField(max_length=12)
-    invoice = models.CharField(max_length=20)
-    inv_date = models.DateField(null=True, blank=True)
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    quantity = models.DecimalField(
+    decl_no = models.CharField(max_length=7, null=True, blank=True)
+    decl_date = models.DateField(default=datetime.date.today)
+    seller = models.CharField(max_length=30, null=True, blank=True)
+    truck = models.CharField(max_length=12, null=True, blank=True)
+    invoice = models.CharField(max_length=20, null=True, blank=True)
+    inv_date = models.DateField(default=datetime.date.today)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    description = models.CharField(max_length=100, null=True, blank=True)
+    qty = models.DecimalField(
         max_digits=9, decimal_places=2, null=True, blank=True)
     measure = models.CharField(
         max_length=5, choices=UNIT, default='PCS'
@@ -74,73 +75,3 @@ class Purchase(models.Model):
     def __str__(self):
         return f'{self.material}, DN{str(self.dn)}'
 
-
-class Materials(models.Model):
-    MATERIALS = [
-        ('PE100', 'PE100'),
-        ('STEEL', 'STEEL'),
-        ('STAINLESS_STEEL', 'STAINLESS STEEL'),
-        ('CI', 'CI'),
-        ('NA', 'NA')
-    ]
-    CATEGORIES = [
-        ('PIPE', 'PIPE'),
-        ('VALVE', 'VALVE'),
-        ('FITTING', 'FITTING'),
-        ('STRAINER', 'STRAINER'),
-        ('DISMANTLING_JOINT', 'DISMANTLING JOINT'),
-        ('OTHER', 'OTHER')
-    ]
-    PNS = [
-        ('8', '8'),
-        ('10', '10'),
-        ('16', '16'),
-        ('20', '20'),
-        ('25', '25'),
-        ('40', '40'),
-        ('65', '65'),
-        ('NA', 'NA')
-    ]
-    name = models.CharField(max_length=50)
-    material = models.CharField(
-        max_length=15, choices=MATERIALS, default='PE100')
-    type = models.CharField(
-        max_length=17, choices=CATEGORIES, default='OTHER'
-    )
-    pn = models.CharField(
-        max_length=17, choices=PNS, default='NA')
-
-    dn = models.IntegerField(null=True)
-    od = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True)
-    feature = models.CharField(max_length=10)
-
-    class Meta:
-        db_table = 'materials'
-        ordering = ['material', 'dn']
-
-    def __str__(self):
-        return f'{self.material}, DN{str(self.dn)}'
-
-
-class Suppliers(models.Model):
-    name = models.CharField(max_length=20)
-
-
-class Nodes(models.Model):
-    name = models.CharField(max_length=20)
-
-
-class Junctions(models.Model):
-    geom = models.PointField()
-    name = models.CharField(max_length=20)
-    make = models.CharField(max_length=50, null=True, blank=True)
-    description = models.CharField(max_length=100, null=True, blank=True)
-    dc_id = models.CharField(max_length=5, null=True, blank=True)
-    elevation = models.DecimalField(max_digits=6, decimal_places=2, null=True)
-
-    class Meta:
-        db_table = 'junctions'
-
-    def __str__(self):
-        return str(self.id)
